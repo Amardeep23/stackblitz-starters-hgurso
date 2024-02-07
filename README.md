dc059a470338b1b089e44f8fb3570da0ea1d3faa
Amardeep23 <81394125+Amardeep23@users.noreply.github.com>
2024-02-07 10:09:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-hgurso
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Amardeep23/stackblitz-starters-hgurso)
---
+++ index.js
@@ -0,0 +1,86 @@
+const sort = (students) => {
+  return students.sort((a, b) => {
+    const total = b.biology + b.chemistry - (a.biology + a.chemistry);
+    if (total !== 0) {
+      return total;
+    }
+
+    const bio = b.biology - a.biology;
+    if (bio !== 0) {
+      return bio;
+    }
+
+    const DOB = new Date(a.dob) - new Date(b.dob);
+    return DOB;
+  });
+};
+
+let testCase1 = [
+  {
+    name: 'Leo Messi',
+    dob: '31-12-1995',
+    biology: 95,
+    chemistry: 91,
+  },
+  {
+    name: 'Cristiano Ronaldo',
+    dob: '31-05-1992',
+    biology: 90,
+    chemistry: 81,
+  },
+  {
+    name: 'Virat Kohli',
+    dob: '31-12-1995',
+    biology: 95,
+    chemistry: 96,
+  },
+  {
+    name: 'Rohit Sharma',
+    dob: '31-12-1995',
+    biology: 85,
+    chemistry: 86,
+  },
+  {
+    name: 'Viswanathan Anand',
+    dob: '12-12-1994',
+    biology: 99,
+    chemistry: 10,
+  },
+];
+
+console.log(sort(testCase1).map((student) => student.name));
+
+let testCase2 = [
+  {
+    name: 'Leo Messi',
+    dob: '31-12-1995',
+    biology: 100,
+    chemistry: 80,
+  },
+  {
+    name: 'Cristiano Ronaldo',
+    dob: '31-05-1992',
+    biology: 80,
+    chemistry: 100,
+  },
+  {
+    name: 'Virat Kohli',
+    dob: '31-12-1995',
+    biology: 30,
+    chemistry: 40,
+  },
+  {
+    name: 'Rohit Sharma',
+    dob: '31-12-1995',
+    biology: 40,
+    chemistry: 30,
+  },
+  {
+    name: 'Viswanathan Anand',
+    dob: '12-12-1994',
+    biology: 99,
+    chemistry: 10,
+  },
+];
+
+console.log(sort(testCase2).map((student) => student.name));
